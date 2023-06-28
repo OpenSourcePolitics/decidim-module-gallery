e6384655eb524c1a990f3778da9d8a4bd65a5b53
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-06-28 23:01:05 +0300

    Display warning when there are no published Gallery items (#11)

--- spec/system/browse_gallery_spec.rb
+++ spec/system/browse_gallery_spec.rb
@@ -12,32 +12,78 @@ describe "Browse gallery", type: :system do
   end
 
   context "with image gallery" do
-    let!(:gallery_component) { create(:gallery_component, :image, :with_items, name: "My gallery block", participatory_space: participatory_space) }
+    context "with no items" do
+      let!(:gallery_component) { create(:gallery_component, :image, name: "My gallery block", participatory_space: participatory_space) }
 
-    it "displays gallery" do
-      expect(page).to have_content(/My gallery block/i)
-      expect(page).to have_selector(".gallery-heading", count: 1)
-      expect(page).to have_selector(".gallery.image", count: 1)
-      within ".gallery.image" do
-        expect(page).to have_selector(".gallery-item", count: 6)
+      it "displays gallery" do
+        expect(page).to have_content(/My gallery block/i)
+        expect(page).to have_selector(".gallery-heading", count: 1)
+        expect(page).to have_selector(".gallery.image", count: 0)
+        expect(page).to have_content(/There are no gallery items/i)
+        expect(page).not_to have_link("View more")
       end
+
+      it_behaves_like "gallery accessible page"
     end
 
-    it_behaves_like "gallery accessible page"
+    context "with items" do
+      let!(:gallery_component) { create(:gallery_component, :image, :with_items, name: "My gallery block", participatory_space: participatory_space) }
+
+      it "displays gallery" do
+        expect(page).to have_content(/My gallery block/i)
+        expect(page).to have_selector(".gallery-heading", count: 1)
+        expect(page).to have_selector(".gallery.image", count: 1)
+        within ".gallery.image" do
+          expect(page).to have_selector(".gallery-item", count: 12)
+        end
+      end
+
+      it "has working pagination" do
+        expect(page).to have_content(/My gallery block/i)
+        expect(page).to have_link("View more")
+        find("#link_gallery__#{gallery_component.id}").click
+        expect(page).to have_selector(".gallery-item", count: 16)
+      end
+
+      it_behaves_like "gallery accessible page"
+    end
   end
 
   context "with video gallery" do
-    let!(:gallery_component) { create(:gallery_component, :video, :with_items, name: "My gallery block", participatory_space: participatory_space) }
+    context "with no items" do
+      let!(:gallery_component) { create(:gallery_component, :video, name: "My gallery block", participatory_space: participatory_space) }
 
-    it "displays gallery" do
-      expect(page).to have_content(/My gallery block/i)
-      expect(page).to have_selector(".gallery-heading", count: 1)
-      expect(page).to have_selector(".gallery.video", count: 1)
-      within ".gallery.video" do
-        expect(page).to have_selector(".gallery-item", count: 6)
+      it "displays gallery" do
+        expect(page).to have_content(/My gallery block/i)
+        expect(page).to have_selector(".gallery-heading", count: 1)
+        expect(page).to have_selector(".gallery.image", count: 0)
+        expect(page).to have_content(/There are no gallery items/i)
+        expect(page).not_to have_link("View more")
       end
+
+      it_behaves_like "gallery accessible page"
     end
 
-    it_behaves_like "gallery accessible page"
+    context "with no items" do
+      let!(:gallery_component) { create(:gallery_component, :video, :with_items, name: "My gallery block", participatory_space: participatory_space) }
+
+      it "displays gallery" do
+        expect(page).to have_content(/My gallery block/i)
+        expect(page).to have_selector(".gallery-heading", count: 1)
+        expect(page).to have_selector(".gallery.video", count: 1)
+        within ".gallery.video" do
+          expect(page).to have_selector(".gallery-item", count: 12)
+        end
+      end
+
+      it "has working pagination" do
+        expect(page).to have_content(/My gallery block/i)
+        expect(page).to have_link("View more")
+        find("#link_gallery__#{gallery_component.id}").click
+        expect(page).to have_selector(".gallery-item", count: 16)
+      end
+
+      it_behaves_like "gallery accessible page"
+    end
   end
 end
