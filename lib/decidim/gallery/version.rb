4162a3bcef19c8e98c5343226b407bfe39a68eae
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-09-29 10:07:45 +0300

    Prepare release

--- lib/decidim/gallery/version.rb
+++ lib/decidim/gallery/version.rb
@@ -4,11 +4,7 @@ module Decidim
   # This holds the decidim-gallery version.
   module Gallery
     def self.version
-      "0.0.4"
-    end
-
-    def self.decidim_version
-      "~> 0.26.0"
+      "0.26.0"
     end
   end
 end
